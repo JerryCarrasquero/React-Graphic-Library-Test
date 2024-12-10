750fa5b1f019244ea38b08d9d963943322fb569d
JerryCarrasquero <jerry.carrasquero@emqu.net>
2024-12-10 15:44:38 -0400

    [Develop] Quick Docummentation for navigation

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPageRouter from "./views/MainPage/MainPageRoutes";
+/* It is using
+the `createBrowserRouter` function to create a browser router for the application. The
+`MainPageRouter` contains the routes for the main page of the application. The
+`createBrowserRouter([...MainPageRouter])` call is passing the routes defined in `MainPageRouter` to
+create the browser router for the application. The `App` variable is then assigned the created
+browser router. */
 
 const App = createBrowserRouter([...MainPageRouter]);
 
--- src/views/MainPage/MainPageRoutes.tsx
+++ src/views/MainPage/MainPageRoutes.tsx
@@ -1,6 +1,10 @@
 import MainPage from "./MainPage";
 import CreateItemPage from "./routes/CreateItemPage";
 import DetailsPage from "./routes/DetailsPage";
+/* This code snippet is defining an array called `MainPageRouter` that contains objects representing
+different routes in a TypeScript React application. Each object in the array has two properties:
+`path` which specifies the route path, and `element` which represents the component to render when
+that route is accessed. */
 
 const MainPageRouter = [
   {
