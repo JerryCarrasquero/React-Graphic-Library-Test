344339b080e381029101273228a0cac574ee2f56
JerryCarrasquero <jerry.carrasquero@emqu.net>
2024-12-12 15:24:01 -0400

    [Develop] Made bar chart component
    Made a bar chart coponent to display campaing information

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -1,8 +1,48 @@
 import { Box } from "@mui/material";
 import BasicPie from "./component/charts/PieChart";
 import BasicBars from "./component/charts/BarChart"
+import { useState } from "react";
 //React
 export default function MainPage() {
+  const [ dataset, setDataset] = useState([
+    {
+      error: 5,
+      delivered: 6,
+      total: 11,
+      campaing: "campaing 1",
+    },
+    {
+      error: 4,
+      delivered: 6,
+      total: 10,
+      campaing: "campaing 2",
+    },
+    {
+      error: 5,
+      delivered: 6,
+      total: 11,
+      campaing: "campaing 3",
+    },
+    {
+      error: 5,
+      delivered: 6,
+      total: 11,
+      campaing: "campaing 4",
+    },
+    {
+      error: 5,
+      delivered: 4,
+      total: 9,
+      campaing: "campaing 5",
+    },
+    {
+      error: 3,
+      delivered: 2,
+      total: 5,
+      campaing: "campaing 6",
+    },
+  ])
+
   return (
     <Box>
       <BasicPie
@@ -14,7 +54,7 @@ export default function MainPage() {
         width={400}
         height={200}
       />
-      <BasicBars/>
+      <BasicBars datas ={dataset} width ={400} height ={500} />
     </Box>
   );
 }
