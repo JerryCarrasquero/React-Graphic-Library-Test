51fd1b06d11b99d7dfb99bdd366c4ed24d04d20d
JerryCarrasquero <jerry.carrasquero@emqu.net>
2024-12-12 15:26:27 -0400

    [Develop] Documentation of function for the graph component

--- src/views/MainPage/component/charts/BarChart.jsx
+++ src/views/MainPage/component/charts/BarChart.jsx
@@ -1,4 +1,11 @@
 import { BarChart } from "@mui/x-charts/BarChart";
+/**
+ * The BasicBars function renders a horizontal bar chart using the provided data and dimensions.
+ * @returns A function component named BasicBars is being returned. It takes in three props: datas
+ * (default value is an empty array), width (default value is 0), and height (default value is 0).
+ * Inside the component, a BarChart component is being rendered with specific dataset, yAxis
+ * configuration, series configuration, layout, width, and height props.
+ */
 
 export default function BasicBars({ datas = [], width = 0, height = 0 }) {
 
--- src/views/MainPage/component/charts/PieChart.jsx
+++ src/views/MainPage/component/charts/PieChart.jsx
@@ -1,4 +1,7 @@
 import { PieChart } from "@mui/x-charts/PieChart";
+/* This code snippet is a React component named `BasicPie` that renders a `PieChart` component from the
+`@mui/x-charts` library. the data prop passes the data required to be displayed 
+and the dimension of the graph can be modified with widht and height */
 
 export default function BasicPie({ datas = [], width = 0, height = 0 }) {
   console.log(datas);
